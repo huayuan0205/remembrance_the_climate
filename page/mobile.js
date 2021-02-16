e8199c93c0d2f3fecb92acd8066ed9fb44d18c1a
houjiang <38600780+JialingJia@users.noreply.github.com>
2021-02-16 00:45:41 -0500

    update

--- page/mobile.js
+++ page/mobile.js
@@ -21,10 +21,10 @@ function bindTouch(el, callBack) {
       //   slider.off('touchmove');
       //   return callBack('left')
       // };
-      if (touch.pageY - startY > 5) {
+      if (touch.pageY - startY > 1) {
         console.log("scroll down");
         return callBack('dowm')
-      } else if (touch.pageY - startY < -5) {
+      } else if (touch.pageY - startY < -1) {
         console.log("scroll up");
         return callBack('up')
       };
